d4398057d1ecf36c7f5f5494c43a8cd94d1a10b9
Adam Conybear <aconybear22@gmail.com>
2020-10-28 15:48:44 -0400

    parsed ical file and connected to mongodb

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Home.css';
 
-function Home() {
+const Home = () => {
+
+    const [open, setOpen] = useState(false);
+
+    const test = () => {
+        // console.log("testing");
+        return(
+            <h2>Testing the test</h2>
+        );
+
+    }
+
+    const handleOpen =(e) => {
+        e.preventDefault();
+        setOpen(true);
+
+    }
+
+
     return (
         <div> 
             <h1> Dashboard goes here</h1>
+            <button onClick={handleOpen}>Open popup</button>
+            {open ? test(): null}
         </div>
     );
 }
