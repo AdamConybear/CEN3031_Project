25bde292a5c120d5914f4a577f8d7417a63ecd29
jeffreysf <>
2020-11-07 19:14:32 -0500

    Added a bunch of new views with their respective js and css files, routed with App

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,11 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Week from "./views/week/weekDash";
 import CommentBoard from "./views/CommentBoard/CommentBoard";
+import Contact from "./views/Contact/Contact";
+import WhatIf from "./views/WhatIf/WhatIf";
+import Resources from "./views/Resources/Resources";
+import AboutUs from "./views/AboutUs/AboutUs";
+import Help from "./views/Help/Help";
 
 const App = () => {
   return (
@@ -14,7 +19,12 @@ const App = () => {
       <Switch>
         <Route path="/Home" component={Home} />
         <Route path="/Week" exact component={Week} />
+        <Route path="/WhatIf" exact component={WhatIf} />
+        <Route path="/Resources" exact component={Resources} />
         <Route path="/CommentBoard" exact component={CommentBoard} />
+        <Route path="/Contact" exact component={Contact} />
+        <Route path="/AboutUs" exact component={AboutUs} />
+        <Route path="/Help" exact component={Help} />
         <Route exact path="/">
           <Redirect to="/Home" />
         </Route>
