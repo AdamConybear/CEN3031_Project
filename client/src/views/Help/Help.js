7bbab8bc42c2326284c2711223a1bfe7c5fc38f9
jeffreysf <>
2020-11-07 19:15:23 -0500

    Forgot to actually add the aforementioned new views

---
+++ client/src/views/Help/Help.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Help.css";
+
+const Help = () => {
+  return <div></div>;
+};
+
+export default Help;
---
+++ client/src/views/Resources/Resources.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Resources.css";
+
+const Resources = () => {
+  return <div></div>;
+};
+
+export default Resources;
