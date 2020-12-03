5ccf0fb98a95cdf1c4a351877d352b7d949b053e
Adam Conybear <aconybear22@gmail.com>
2020-12-03 11:18:26 -0500

    addded hashrouter and butter route

--- server/config/express.js
+++ server/config/express.js
@@ -41,6 +41,9 @@ module.exports.init = () => {
   app.use('/api/popups', popupRouter);
   app.use('/api/comment', commentRouter);
   app.use('/api/week', weekRouter);
+  app.get("/butter"), function (req, res) {
+    res.end("Testing butter route")
+  };
 
   if (process.env.NODE_ENV === "production") {
     // Serve any static files
@@ -49,9 +52,7 @@ module.exports.init = () => {
     app.get("/*", function (req, res) {
       res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
-    app.get("/butter"), function (req, res) {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    };
+    
   }
 
   return app;
