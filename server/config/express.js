0fc0a5d1bd028b32c83fcd7a0308b0c88581c206
Adam Conybear <aconybear22@gmail.com>
2020-12-02 20:59:15 -0500

    no changes

--- server/config/express.js
+++ server/config/express.js
@@ -46,12 +46,6 @@ module.exports.init = () => {
     // Serve any static files
     app.use(express.static(path.join(__dirname, "../../client/build")));
     // app.use(express.static('client/build'));
-
-    // Handle React routing, return all requests to React app
-    // app.get("*", function (req, res) {
-    //   res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
-    // });
-
     app.get("*", function (req, res) {
       res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
