fb293a566a6e7fd40446158d818bb68446e5b8a7
Adam Conybear <aconybear22@gmail.com>
2020-12-07 01:29:11 -0500

    hard coding auth0 creds

--- client/src/authDev copy.js
+++ client/src/authDev copy.js
@@ -1,7 +1,4 @@
 module.exports = {
     domain: "dev--xuoc9q7.auth0.com",
     clientId: "0xZ1LlUnDHzjqAnAyKtrUaZpFpMlI4gi"
-
-    // domain: process.env.AUTH_DOMAIN,
-    // clientId: process.env.AUTH_CLIENTID
 };
